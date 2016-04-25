5acf30643c6f3396b01edd383b240ad435e003be
ahwhitakerkck <ahwhitakerkck@yahoo.com>
2016-04-25 10:01:54 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -33,3 +33,9 @@ var featureLayer = L.mapbox.featureLayer()
       map.fitBounds(featureLayer.getBounds());
     })
     
+    
+    featureLayer.on('ready' , function(){
+      	this.eachLayer(function(layer){
+          	layer.bindPopup('Welcome to ' + layer.feature.properties.LABEL);
+        })
+    })
